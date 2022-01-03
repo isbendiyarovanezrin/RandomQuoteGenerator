d0470ea03fc176761eee7743f39d4f25a4150559
Nəzrin <isbndyrva@gmail.com>
2022-01-03 15:41:48 +0400

    Add random quote generator

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById("btn");
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+
+fetch("https://type.fit/api/quotes")
+  .then((response) => response.json())
+  .then((quotes) =>
+    btn.addEventListener("click", () => {
+      const random = Math.floor(Math.random() * quotes.length);
+      quote.textContent = quotes[random].text;
+      author.textContent = `-${quotes[random].author}`;
+      changeColor();
+    })
+  )
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+function changeColor() {
+  let randomColor = "#" + ((Math.random() * 16777215) | 0).toString(16);
+  $("#btn").hover(
+    function () {
+      $(this).css("color", randomColor);
+    },
+    function () {
+      $(this).css("color", "");
+    }
+  );
+  $("body, .quotes, #btn").css("background-color", randomColor);
+}
+
+// loader
+function removeLoader() {
+  $("#loadingDiv").fadeOut(500, () => {
+    $("#loadingDiv").remove();
+  });
+}
+
+$(window).on("load", () => {
+  setTimeout(removeLoader, 2000);
+});
