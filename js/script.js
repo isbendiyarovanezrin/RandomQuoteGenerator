e7796257a25edd6f57db378bb805e77436ef6d70
Nəzrin <isbndyrva@gmail.com>
2022-04-16 22:20:27 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,8 +19,57 @@ fetch("https://type.fit/api/quotes")
   });
 
 function changeColor() {
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  $("body, .quotes, #btn").css("background-color", randomColor);
+  const colors = [
+    "#eeee90",
+    "#90eebf",
+    "#FDDF9E",
+    "#9B59B6",
+    "#ffd2a5",
+    "#9090ee",
+    "#beacc6",
+    "#a6f1a6",
+    "#EAD7C3",
+    "#ECC1D1",
+    "#C8D3B8",
+    "#C3EFD9",
+    "#008080",
+    "#bfee90",
+    "#ee90ee",
+    "#d2a5ff",
+    "#EFC3EF",
+    "#CCFFCC",
+    "#A6C4E0",
+    "#bcf5bc",
+    "#D99197",
+    "#C3EFC3",
+    "#40E0D0",
+    "#a5d2ff",
+    "#D9B4E2",
+    "#3CB371",
+    "#B4D5AC",
+    "#bf90ee",
+    "#90ee90",
+    "#C3D0EF",
+    "#D9C3EF",
+    "#00CED1",
+    "#B4D7A2",
+    "#E8DFD5",
+    "#A4BCC0",
+    "#F7E1EA",
+    "#CCB2B3",
+    "#FA87B1",
+    "#F8C8DA",
+    "#79BD8F",
+    "#ffdfbf",
+    "#F3A7C6",
+    "#59CF8D",
+    "#EFC3D9",
+  ];
+
+  $("body, .quotes, #btn").css(
+    "background-color",
+    colors[Math.floor(Math.random() * colors.length)]
+  );
 }
 
 // loader
